148c056b1ec7441577a2358d9801774fdc882ead
twenty3 <chris@twenty3.me>
2014-08-25 21:00:11 -0600

    Initial Commit

---
+++ Fling Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fling Demo
+//
+//  Created by 23 on 8/25/14.
+//  Copyright (c) 2014 Aged and Distilled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fling Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Fling Demo
+//
+//  Created by 23 on 8/25/14.
+//  Copyright (c) 2014 Aged and Distilled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
